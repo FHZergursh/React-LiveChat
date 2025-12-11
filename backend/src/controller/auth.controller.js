45f1b8d47005bc3b228b2d3928441f6c0f31880d
Flinn Alexnader Hancock <89704618+FHZergursh@users.noreply.github.com>
2025-12-11 05:17:48 +0000

    create user model & signup API
    title

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,5 +1,59 @@
+import User from '../models/User.js'
+import bcrypt from 'bcryptjs' 
+import { generateToken } from '../lib/utils.js'
+
 export const signup = async (req, res) => {
-  res.send("Singup endpoint");
+  const {fullName, email, password} = req.body
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({message:"All fields required"})
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({message:"Password needs to be 6 character"})
+    }
+
+    // check if email valid
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const user = await User.findOne({email})
+    if (user) return res.status(400).json({message: "Email already exists"})
+    
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword
+    })
+
+    if (newUser) {
+      generateToken(newUser._id, res)
+      await newUser.save()
+
+      res.status(201).json({
+        _id:newUser._id,
+        fullName:newUser.fullName,
+        email:newUser.email,
+        profilePic:newUser.profilePic
+      })
+
+    } else {
+      res.status(400).json({message: "Invalid user data"})
+    }
+
+    res.status(201).json({newUser})
+
+
+  } catch (error) {
+    console.log("Error in signup controller", error)
+    res.status(500).json({message: "Internal server error!"})
+
+  }
 }
 
 export const login = async (req, res) => {
